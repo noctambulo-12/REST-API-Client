d3f6506cbb8f5db925c89619b9500e3cc52ca679
Elmer <elmer@cangahuala.dev>
2021-08-12 00:14:53 -0500

    Adding project consume api rest with RestSharp

---
+++ ConsoleAPIClient.RestSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAPIClient.RestSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
